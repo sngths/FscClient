5e75033d0381f014633d940646e7380e4d3fc9de
tianxing <1035273486@qq.com>
2016-07-06 14:47:07 +0800

    创建载入界面和启动初始化

--- app/src/main/java/com/tianxing/view/activity/LoadingActivity.java
+++ app/src/main/java/com/tianxing/view/activity/LoadingActivity.java
@@ -1,5 +1,6 @@
 package com.tianxing.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.KeyEvent;
@@ -21,11 +22,50 @@ public class LoadingActivity extends BaseActivity {
 
 
     @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus){
+            //界面启动完成 创建初始化线程
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+
+                    try {
+                        Thread.currentThread().sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //初始化完成 跳转界面
+                            startMainActivity();
+                        }
+                    });
+                }
+            }).start();
+        }
+    }
+
+    @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         //super.onKeyDown(keyCode, event);
+        //拦截返回按键
         if (keyCode == KeyEvent.KEYCODE_BACK){
 
         }
         return true;
     }
+
+
+
+    private void startMainActivity(){
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        this.finish();
+    }
+
+
 }
--- app/src/main/java/com/tianxing/view/activity/MainActivity.java
+++ app/src/main/java/com/tianxing/view/activity/MainActivity.java
@@ -3,5 +3,5 @@ package com.tianxing.view.activity;
 /**
  * Created by tianxing on 16/7/5.
  */
-public class MainActivity {
+public class MainActivity extends BaseActivity {
 }
